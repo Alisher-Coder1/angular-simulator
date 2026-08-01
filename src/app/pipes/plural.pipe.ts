2d1a141ee05e27b7514fe22da903cf8098bc6d4b
Alisher <adododzhonov@inbox.ru>
2026-08-01 17:21:57 +0300

    feat: add custom pipes and directives

---
+++ src/app/pipes/plural.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'plural',
+})
+export class PluralPipe implements PipeTransform {
+  public transform(
+    value: number,
+    one: string,
+    few: string,
+    many: string,
+  ): string {
+    const normalizedValue = Math.abs(Math.trunc(value));
+    const lastTwoDigits = normalizedValue % 100;
+    const lastDigit = normalizedValue % 10;
+
+    if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+      return many;
+    }
+
+    if (lastDigit === 1) {
+      return one;
+    }
+
+    if (lastDigit >= 2 && lastDigit <= 4) {
+      return few;
+    }
+
+    return many;
+  }
+}
